e24d68210e202e322e310aff4aaff43bf970efdb
Nebojsa Brankovic <n.brankovic99@gmail.com>
2023-07-04 21:07:33 +0200

    Uradjena kontakt stranica sa propratnim css-om

---
+++ fitness-react/src/components/Kontakt.jsx
@@ -0,0 +1,23 @@
+import './KontaktStyle.css';
+
+function Kontakt() {
+    function handleSend() {
+        alert("Poruka je uneta.")
+    }
+
+    return (
+        <div className="container">
+            <form onSubmit={handleSend}>
+                <label htmlFor="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+                <label htmlFor="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+                <label htmlFor="subject">Subject</label>
+                <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+}
+
+export default Kontakt;
