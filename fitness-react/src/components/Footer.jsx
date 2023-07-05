0c4284c07106283917b40c2810e54790955d7234
Nebojsa Brankovic <n.brankovic99@gmail.com>
2023-07-05 16:31:07 +0200

    Skracen css i izbaceni neki nepotrebni divovi

--- fitness-react/src/components/Footer.jsx
+++ fitness-react/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="bg-light text-center text-lg-start">
+        <footer className="footer">
             <div className="text-center p-3">
                 © 2023 Copyright: 
                 <a href="https://github.com/BrankovicNebojsa"> Nebojsa Brankovic</a>
--- fitness-react/src/components/Navbar.jsx
+++ fitness-react/src/components/Navbar.jsx
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
-        <div >
-            <div className="topnav">
-                <Link to="/">Početna strana</Link>
-                <Link to="/proteini">Proteini</Link>
-                <Link to="/kontakt">Kontakt strana</Link>
-            </div>
+        <div className="topnav">
+            <Link to="/">Početna strana</Link>
+            <Link to="/proteini">Proteini</Link>
+            <Link to="/kontakt">Kontakt strana</Link>
         </div>
     );
 }
